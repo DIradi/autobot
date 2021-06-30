0f2b5dcbbd15282ca1fd045d188cb3c52c16febc
DESKTOP-MOFE152\Diego <diego.iradi@gmail.com>
2021-06-29 22:04:48 -0400

    v3

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Container} from "react-bootstrap"
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=8e7fbc4daa5b40f3a2486b16ee6ab66e&response_type=code&redirect_uri=https://spotify-ser.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=8e7fbc4daa5b40f3a2486b16ee6ab66e&response_type=code&redirect_uri=https://spotify-cliente.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 
 export default function Login() {
